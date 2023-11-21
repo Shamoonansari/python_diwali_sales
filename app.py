630bb046c57ae98109b1aab37c34081158312926
sandeep Github <sandeeprairai339@gmail.com>
2023-11-21 15:40:24 +0530

    one extra function

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ def add(self,a,b):
     return a+b
 def substract(self,a,b):
     return a-b
-
+def mulyiply(self,a,b):
+    return a*b
 
 a=int(input("Enter the 1st number"))
 b=int(input("Enter the 2nd number"))
